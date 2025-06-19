8a7ee2cea088aeb3486cdeecbfcfa7d11f33193a
otavioaugusto1 <otavio.a.nogueira@hotmail.com>
2025-06-19 20:05:40 -0300

    Adição do código base, explicação de comentarios, variaveis e macros

---
+++ meu_primeiro_rust/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Teste 2!");
+    println!("teste 3!");
+    print!("Não quebra!");
+    print!("linha!");
+
+    // Isso é um comentário
+
+    let nome = "Otávio";
+    println!("O meu nome é: {}", nome);
+
+}
