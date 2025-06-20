c7bb16bf1899465f2f5c37e137dcc5ffb9fba24e
otavioaugusto1 <otavio.a.nogueira@hotmail.com>
2025-06-19 21:12:18 -0300

    Adição do código base, explicação de comentarios, variaveis e macros

--- meu_primeiro_rust/src/main.rs
+++ meu_primeiro_rust/src/main.rs
@@ -10,4 +10,26 @@ fn main() {
     let nome = "Otávio";
     println!("O meu nome é: {}", nome);
 
+    let my_num = 5;         // integer
+    let my_double = 5.99;   // float
+    let my_letter = 'D';    // character
+    let my_bool = true;     // boolean
+    let my_text = "Hello";  // string
+
+    // Posso declarar dessa forma
+    println!("{}",my_num);
+    println!("{}",my_text);
+    println!("{}",my_letter);
+    println!("{}",my_bool);
+    println!("{}",my_double);
+
+    // Ou dessa forma:
+    /* 
+    let my_num: i32 = 5;          // integer
+    let my_double: f64 = 5.99;    // float
+    let my_letter: char = 'D';    // character
+    let my_bool: bool = true;     // boolean
+    let my_text: &str = "Hello";  // string 
+    // */
+
 }
